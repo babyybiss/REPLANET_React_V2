d1b4f869a43cb784481985c574620f8a0ebc2415
Gray-Grazer <cleit8307@gmail.com>
2023-11-07 19:02:47 +0900

    선형 chart  컴포넌트 추가, 막대형 chart 마우스 이벤트 추가

---
+++ src/component/charts/TestChart.js
@@ -0,0 +1,89 @@
+import {VictoryContainer, VictoryBar, VictoryChart, VictoryAxis} from 'victory';
+import "../../assets/css/chart.css";
+
+function TestChart() {
+
+    const CurrentYearData = [
+        {
+          monthly: 1, campaings: 12, label: ""
+        },
+        {
+          monthly: 2, campaings: 20, label: ""
+        },
+        {
+          monthly: 3, campaings: 20, label: ""
+        },
+        {
+          monthly: 4, campaings: 34, label: ""
+        },
+        {
+          monthly: 5, campaings: 15, label: ""
+        },
+        {
+          monthly: 6, campaings: 34, label: ""
+        },
+        {
+          monthly: 7, campaings: 22, label: ""
+        },
+        {
+          monthly: 8, campaings: 5, label: ""
+        },
+        {
+          monthly: 9, campaings: 6, label: ""
+        },
+        {
+          monthly: 10, campaings: 8, label: ""
+        },
+        {
+          monthly: 11, campaings: 9, label: ""
+        },
+        {
+          monthly: 12, campaings: 8, label: ""
+        }
+      ];
+
+      const PreviousYearData = [
+        {
+          monthly: 1, campaings: 12, label: ""
+        },
+        {
+          monthly: 2, campaings: 20, label: ""
+        },
+        {
+          monthly: 3, campaings: 20, label: ""
+        },
+        {
+          monthly: 4, campaings: 34, label: ""
+        },
+        {
+          monthly: 5, campaings: 15, label: ""
+        },
+        {
+          monthly: 6, campaings: 34, label: ""
+        },
+        {
+          monthly: 7, campaings: 22, label: ""
+        },
+        {
+          monthly: 8, campaings: 5, label: ""
+        },
+        {
+          monthly: 9, campaings: 6, label: ""
+        },
+        {
+          monthly: 10, campaings: 8, label: ""
+        },
+        {
+          monthly: 11, campaings: 9, label: ""
+        },
+        {
+          monthly: 12, campaings: 8, label: ""
+        }
+      ];
+
+
+
+
+
+}
+export default TestChart;
--- src/component/common/Sidebar.js
+++ src/component/common/Sidebar.js
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
+import "../../assets/css/reset.css"
+import "../../assets/css/common.css"
+
 
 function Sidebar() {
     return(
-        <div>
-            <ul>
-                <li><NavLink to="/">캠페인 관리</NavLink></li>
-                <li><NavLink to="/">포인트 전환 및 관리</NavLink></li>
-                <li><NavLink to="/">후기 관리</NavLink></li>
-                <li><NavLink to="/charts">캠페인 현황 통계</NavLink></li>
+        <div className="admin-sidebar">
+            <ul className="admin-sidebar">
+                <li className="admin-sidebar-menu"><NavLink to="/">캠페인 관리</NavLink></li>
+                <li className="admin-sidebar-menu"><NavLink to="/">포인트 전환 및 관리</NavLink></li>
+                <li className="admin-sidebar-menu"><NavLink to="/">후기 관리</NavLink></li>
+                <li className="admin-sidebar-menu"><NavLink to="/charts">캠페인 현황 통계</NavLink></li>
             </ul>
         </div>
     );
