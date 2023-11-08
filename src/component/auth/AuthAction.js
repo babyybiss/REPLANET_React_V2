1c5b91e78d6f8016ef6360206e77dccaa9deb1ae
vaguewords <vaguewords@gmail.net>
2023-11-08 18:04:56 +0900

    로그인 오류 수정
    로그인 오류 수정

--- src/component/auth/AuthAction.js
+++ src/component/auth/AuthAction.js
@@ -1,76 +1,61 @@
-import { GET, POST } from "../../apis/AuthAPI"
-
-const createTokenHeader = token => {
+import { GET, POST } from "../../apis/AuthAPI";
+const createTokenHeader = (token) => {
   return {
-    headers: {
-      Authorization: "Bearer " + token
-    }
-  }
-}
-
-const calculateRemainingTime = expirationTime => {
-  const currentTime = new Date().getTime()
-  const adjExpirationTime = new Date(expirationTime).getTime()
-  const remainingDuration = adjExpirationTime - currentTime
-  return remainingDuration
-}
-
+      headers: {
+          'Authorization': 'Bearer ' + token
+      }
+  };
+};
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+  const remainingDuration = adjExpirationTime - currentTime;
+  return remainingDuration;
+};
 export const loginTokenHandler = (token, expirationTime) => {
-  localStorage.setItem("token", token)
-  localStorage.setItem("expirationTime", String(expirationTime))
-
-  const remainingTime = calculateRemainingTime(expirationTime)
-  return remainingTime
-}
-
+  localStorage.setItem('token', token);
+  localStorage.setItem('expirationTime', String(expirationTime));
+  const remainingTime = calculateRemainingTime(expirationTime);
+  return remainingTime;
+};
 export const retrieveStoredToken = () => {
-  const storedToken = localStorage.getItem("token")
-  const storedExpirationDate = localStorage.getItem("expirationTime") || "0"
-
-  const remaingTime = calculateRemainingTime(+storedExpirationDate)
-
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationDate = localStorage.getItem('expirationTime') || '0';
+  const remaingTime = calculateRemainingTime(+storedExpirationDate);
   if (remaingTime <= 1000) {
-    localStorage.removeItem("token")
-    localStorage.removeItem("expirationTime")
-    return null
+      localStorage.removeItem('token');
+      localStorage.removeItem('expirationTime');
+      return null;
   }
-
   return {
-    token: storedToken,
-    duration: remaingTime
-  }
-}
-
+      token: storedToken,
+      duration: remaingTime
+  };
+};
 export const signupActionHandler = (email, password, memberName, phone) => {
-  const URL = "/auth/signup"
-  const signupObject = { email, password, memberName, phone }
-
-  const response = POST(URL, signupObject, {})
-  return response
-}
-
+  const URL = '/auth/signup';
+  const signupObject = { email, password, memberName, phone };
+  const response = POST(URL, signupObject, {});
+  return response;
+};
 export const loginActionHandler = (email, password) => {
-  const URL = "/auth/login"
-  const loginObject = { email, password }
-  const response = POST(URL, loginObject, {})
-
-  return response
-}
-
+  const URL = '/auth/login';
+  const loginObject = { email, password };
+  const response = POST(URL, loginObject, {});
+  return response;
+};
 export const logoutActionHandler = () => {
-  localStorage.removeItem("token")
-  localStorage.removeItem("expirationTime")
-}
-
-export const getUserActionHandler = token => {
-  const URL = "/member/me"
-  const response = GET(URL, createTokenHeader(token))
-  return response
-}
-
-export const changePasswordActionHandler = (email, exPassword, newPassword, token) => {
-  const URL = "/member/password"
-  const changePasswordObj = { email, exPassword, newPassword }
-  const response = POST(URL, changePasswordObj, createTokenHeader(token))
-  return response
-}
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationTime');
+};
+export const getUserActionHandler = (token) => {
+  const URL = '/';
+  const response = GET(URL, createTokenHeader(token));
+  return response;
+};
+export const changePasswordActionHandler = (exPassword, newPassword, token) => {
+  const URL = '/';
+  const changePasswordObj = { exPassword, newPassword };
+  const response = POST(URL, changePasswordObj, createTokenHeader(token));
+  return response;
+};
