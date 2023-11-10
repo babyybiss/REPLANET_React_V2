e1b78ebf86715fa899bf659c53e42ad7277b6ba1
vaguewords <vaguewords@gmail.net>
2023-11-10 15:33:38 +0900

    .env 추가, 카카오 로그인 수정
    .env 추가, 카카오 로그인 수정

--- src/pages/auth/KakaoLogin.js
+++ src/pages/auth/KakaoLogin.js
@@ -1,23 +1,17 @@
 import React, { useState, useRef, useContext } from "react";
 import '../../assets/css/user.css';
 
-const KakaoLogin = () => {
-
-    const CLIENT_ID = '5a90ac83e3e59ba52ef0c472bc65e3e0';
-    const REDIRECT_URI = 'http://localhost:8001/login/oauth2/code/kakao';
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+function KakaoLogin() {
+    const client_id = process.env.REACT_APP_KAKAO_CLIENT_ID;
+    const redirect_uri = process.env.REACT_APP_KAKAO_REDIRECT_URI;
   
-    const KakaoLoginHandler = () => {
-        window.open(KAKAO_AUTH_URL, "_blank", "noopener, noreferrer");
-    };
-
-    const code = new URL(window.location.href).searchParams.get("code");
-
-    return (
-
-        <></>
-    )
-}
+    const url = `https://kauth.kakao.com/oauth/authorize?scope=account_email&client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&prompt=login`;
+  
+    const loginKaKao = () => {
+      window.open(url, "_blank", "noopener, noreferrer");
+    }
+    return (<div className="tab_item ti2" onClick={loginKaKao}>소셜 로그인</div>)  }
+  
 
 
 export default KakaoLogin;
