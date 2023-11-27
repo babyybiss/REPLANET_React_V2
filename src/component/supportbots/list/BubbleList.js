6d0d3653a43fee16f81d9bfb1270d74ee71c9246
Gray-Grazer <cleit8307@gmail.com>
2023-11-27 22:02:20 +0900

    서포트봇 화면구성 완료

--- src/component/supportbots/list/BubbleList.js
+++ src/component/supportbots/list/BubbleList.js
@@ -1,93 +1,92 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-/*
-import FirstResBubble from "../items/responseitem/FirstResBubble";
-import SecondResBubble from "../items/responseitem/SecondResBubble";
-import ThirdResBubble from "../items/responseitem/ThirdResBubble";
-import FourthResBubble from "../items/responseitem/FourthResBubble";
-import FifthResBubble from "../items/responseitem/FifthResBubble";
-import AnyQuestion from "../items/textitem/AnyQuestion";
-*/
+
 
 function BubbleList() {
     const callApiResult = useSelector(state => state.supportbotReducer)
     const supportbotDataList = callApiResult.supportbotDataList.results.allSupportData;
-
     
-    const questionCodeArray = supportbotDataList.map((code) => code.questionCode)
-    const questionContentArray = supportbotDataList.map((question) => question.questionContent) 
-    const answerArray = supportbotDataList.map((answer) => answer.answerContent)
-    /* answerArray = [0,1,2,3,4] */ 
+    const questionContentArray = supportbotDataList.map((question) => question.questionContent)
+    const answerArray = supportbotDataList.map((answer) => answer.answerContent) 
 
     /* 말풍선 리스트 상태 */
     const [bubbles, setBubbles] = useState(supportbotDataList)
-    const [middleBubbles, setMiddleBubbles] = useState([]);
-    
-    const [questionCode, setQuestionCode] = useState(0);
     const [madeKey, setMadekey] = useState(6);
-    const [answer, setAnswer] = useState("");
+    const [anyQuestion, setAnyQuestion] = useState(false)
+
+    const scrollRef = useRef();
+
+    const scrollToBottom = () => {
+        if(scrollRef.current) {
+            scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+        }
+    };
 
     useEffect(() => {
-        console.log(middleBubbles)
-
-
-    },[middleBubbles]);
-
-
-    /* 질문을 눌렀을 경우 기존 말풍선 리스트에 답변 말풍선을 추가하고 싶음 */ 
-    const testFilterBubbleHandler = () => {
-
-
-    }
-
-    const testQuestionClickHandler = () => {
+        scrollToBottom();
+    },[bubbles]);
+    
+    /* 질문을 눌렀을 경우 기존 말풍선 리스트에 답변 말풍선을 추가하고 싶음 */
+    let requestCssClass = "requestBoxClass"
+    let responseCssClass = "responseBoxClass"
+    
+    const testQuestionClickHandler = (questionCode) => {
         
         const changeBubbles = bubbles.concat({
             key: madeKey,
             questionCode: madeKey,
-            questionContent: "가",
-            answerContent: "나"
+            questionContent: questionContentArray[questionCode-1],
+            cssCondition: requestCssClass
+        },
+        {
+            key: madeKey + 1,
+            questionCode: madeKey + 1,
+            questionContent: answerArray[questionCode-1],
+            cssCondition: responseCssClass
         });
-        console.log(changeBubbles)
-        setMadekey(madeKey + 1)
-        setMiddleBubbles(changeBubbles);
+        // console.log(changeBubbles);
+        setMadekey(madeKey + 3);
+        setBubbles(changeBubbles);
+        setAnyQuestion(true);
+        scrollToBottom();
     }
-    
-    
-    /*
-    const selectRenderComponents = () => {
-        switch (questionCode) {
-            case 1:
-                return <FirstResBubble questionCode={questionCode} />;
-            case 2:
-                return <SecondResBubble questionCode={questionCode} />;
-            case 3:
-                return <ThirdResBubble questionCode={questionCode} />;
-            case 4:
-                return <FourthResBubble questionCode={questionCode} />;
-            case 5:
-                return <FifthResBubble questionCode={questionCode} />;
-            default:
-                return null;
-        }
-    }
-    */
-    
 
+    /* 다른 질문하기를 놀렀을 경우 초기 상태로 되돌리고 싶음; */
+    const testResetClickHandler = () => {
+        setAnyQuestion(false)
+        setBubbles(supportbotDataList)
+    }
+    
     return(
-        <>
+        <div 
+            className="modal-scroll-box"
+            ref={scrollRef}
+        >
+            <div className="modal-intro">
+            {`같이의 가치 :) 작은 행동으로도 지구에 변화를 줄 수 있다! 안녕하세요 리플래닛입니다. 무엇을 도와드릴까요?`}
+            </div>
             {bubbles.map((question) => (
                 <div
                     key={question.questionCode}
-                    className="click-box"
-                    onClick={testQuestionClickHandler}
+                    className={
+                        question.cssCondition === requestCssClass ? "requset-box" : 
+                        question.cssCondition === responseCssClass ? "response-box" : "click-box"
+                    }
+                    onClick={() => testQuestionClickHandler(question.questionCode)}
                 >
                     {question.questionContent}
-                </div>
-                
+                </div> 
             ))}
-
-        </>
+            {anyQuestion && 
+                <div
+                    className="reset-box"
+                    onClick={() => testResetClickHandler()}
+                >
+                    다른 질문을 하고 싶어요
+                </div>
+            }
+        </div>
+        
     );
 
 }
