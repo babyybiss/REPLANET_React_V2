38c2950fcda0033db344c3775b13305e7ad68519
Gray-Grazer <cleit8307@gmail.com>
2023-11-27 14:53:29 +0900

    서포트봇 화면구성중

--- src/component/supportbots/list/BubbleList.js
+++ src/component/supportbots/list/BubbleList.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 /*
 import FirstResBubble from "../items/responseitem/FirstResBubble";
@@ -9,43 +9,51 @@ import FifthResBubble from "../items/responseitem/FifthResBubble";
 import AnyQuestion from "../items/textitem/AnyQuestion";
 */
 
-
 function BubbleList() {
     const callApiResult = useSelector(state => state.supportbotReducer)
     const supportbotDataList = callApiResult.supportbotDataList.results.allSupportData;
 
+    
     const questionCodeArray = supportbotDataList.map((code) => code.questionCode)
     const questionContentArray = supportbotDataList.map((question) => question.questionContent) 
     const answerArray = supportbotDataList.map((answer) => answer.answerContent)
-
+    /* answerArray = [0,1,2,3,4] */ 
 
     /* 말풍선 리스트 상태 */
-    const [bubbles, setBubbles] = useState([
-        {
-            questionCode: 0,
-            questionContent : "질문을 선택해주세요",
-            answerContent: ""
-        }
-    ])
-    /* 질문코드 상태 */
-    // const [questionCode, setQuestionCode] = useState(0);
+    const [bubbles, setBubbles] = useState(supportbotDataList)
+    const [middleBubbles, setMiddleBubbles] = useState([]);
+    
+    const [questionCode, setQuestionCode] = useState(0);
+    const [madeKey, setMadekey] = useState(6);
+    const [answer, setAnswer] = useState("");
 
-    /* 
-    const questionClickHandler = (questionCode) => {
-        setQuestionCode(questionCode)
-    }
-    */
+    useEffect(() => {
+        console.log(middleBubbles)
+
+
+    },[middleBubbles]);
+
 
     /* 질문을 눌렀을 경우 기존 말풍선 리스트에 답변 말풍선을 추가하고 싶음 */ 
-    /*
-    const testQuestionClickHandler = () => {
-        const changeBubbles = bubbles.concat({
-            questionCode: questionCode,
-            questionContent: 
-        })
-    }
-    */
+    const testFilterBubbleHandler = () => {
 
+
+    }
+
+    const testQuestionClickHandler = () => {
+        
+        const changeBubbles = bubbles.concat({
+            key: madeKey,
+            questionCode: madeKey,
+            questionContent: "가",
+            answerContent: "나"
+        });
+        console.log(changeBubbles)
+        setMadekey(madeKey + 1)
+        setMiddleBubbles(changeBubbles);
+    }
+    
+    
     /*
     const selectRenderComponents = () => {
         switch (questionCode) {
@@ -64,23 +72,21 @@ function BubbleList() {
         }
     }
     */
+    
 
     return(
         <>
-            {/*
-            {supportbotDataList.map((question) => (
+            {bubbles.map((question) => (
                 <div
                     key={question.questionCode}
                     className="click-box"
-                    onClick={() => questionClickHandler(question.questionCode)}
+                    onClick={testQuestionClickHandler}
                 >
                     {question.questionContent}
                 </div>
                 
             ))}
-            {selectRenderComponents()}
-            <AnyQuestion />
-            */}
+
         </>
     );
 
