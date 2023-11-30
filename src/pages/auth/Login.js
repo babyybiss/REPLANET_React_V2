f01be6fa80cc4e8d46a8880c8b76e607c85d096e
vaguewords <vaguewords@gmail.net>
2023-11-30 17:54:53 +0900

    문자인증 완료 및 정규식 보완
    id/pw 찾기 진행중

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -50,14 +50,14 @@ const Login = () => {
     }
 
     const KakaoLoginHandler = () => {
-        console.log("반갑다 나 카카오다.")
+        console.log("kakao login form")
 
         const REST_API_KEY = "8a5a93627a69a5b1728721bc6ff53635";
         const REDIRECT_URI = "http://localhost:3000/";
     
         const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&scope=account_email&prompt=login`;
     
-        window.location.href = url;
+        window.open(url, "_blank");
     }
 
 
@@ -74,7 +74,7 @@ const Login = () => {
                 <div className="items-container ic1">
                     <div className="tabs">
                     <div className="tab_item ti2 active" >일반 로그인</div>
-                    <div className="tab_item ti2" onClick={GoogleLoginButton}>소셜 로그인</div>
+                    {/* <div className="tab_item ti2" onClick={GoogleLoginButton}>소셜 로그인</div> */}
                     <div className="tab_item ti2" onClick={KakaoLoginHandler}>카카오테스트</div>
                     </div>
 
